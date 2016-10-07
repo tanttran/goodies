5eb94656d8a054893ac472fd564993e2937ee79c
tantran <octttran@gmail.com>
2016-10-06 19:07:15 -0700

    added password encryption to user login #2

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var jwt = require('jwt-simple');
+var bcrypt = require('bcryptjs');
 
 var JWT_SECRET = 'goodies';
 
@@ -30,34 +31,50 @@ var authorized = function (req, res, next) {
 
 
 router.post('/signup', function(req, res, next){
-  var newUser = new User({
-    username: req.body.username,
-    password: req.body.password,
-    email: req.body.email 
-  });
-  newUser.save(function(err) {
-    if (err) return res.status(400).send(err);
-    return res.send();
+  bcrypt.genSalt(10, function(err, salt) {
+    console.log(salt);
+    bcrypt.hash(req.body.password, salt, function(err, hash) {
+      console.log(hash);
+
+      var newUser = new Users({ 
+        username: req.body.username,
+        password: hash,
+        email: req.body.email 
+      });
+
+      newUser.save(function(err) {
+        if (err) return res.status(400).send(err);
+        return res.send();
+      });
+
+    });
   });
 });
+
+
 
 router.put('/login', function(req, res, next) {
-  Users.findOne({username: req.body.username, password: req.body.password}, function(err, user) {
-    if(user) {
-        var mytoken = jwt.encode(user, JWT_SECRET);
-        return res.json({token: mytoken});
-      } 
-      if (err) {
-        return res.status(500).send();
-      }
-
-      if(!user) {
-        return res.status(400).send();
-      }
-
-      return res.status(200).send();
+  Users.findOne({username: req.body.username}, function(err, user) {
+    if(!user) {
+          return res.status(400).send();
+        }   
+        bcrypt.compare(req.body.password, user.password, function(err, isMatch){
+        if(isMatch) {
+          var mytoken = jwt.encode(user, JWT_SECRET);
+          return res.json({token: mytoken});
+        } 
+        if(!isMatch){
+          return res.status(400).send();
+        }
+        if(err) {
+          throw err;
+        } else {
+          return res.status(401).send
+        }
+      });
+    });
   });
-});
+
 
 
 
